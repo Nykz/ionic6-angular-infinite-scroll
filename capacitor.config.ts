21e063c0f59ccadf4586b10b88a8f78453ce7616
Nykz <nykz786@gmail.com>
2022-07-31 17:44:08 +0530

    first commit
    Ionic 6 Angular - Infinite Scroll

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'infinite-scoll-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
